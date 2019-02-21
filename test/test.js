041c81b837badcf3ba0e95f7332e097e3d158eba
Matt Moore <matt.e.moore@gmail.com>
2019-02-21 09:10:34 -0600

    Playing around

--- test/test.js
+++ test/test.js
@@ -10,6 +10,7 @@ afterEach(() => {
 describe("routes: index", () => {
   test("should respond as expected", async () => {
     const response = await request(server).get("/");
+    console.log(response)
     expect(response.status).toEqual(200);
     expect(response.type).toEqual("text/plain");
     expect(response.text).toEqual("Hello World!");
